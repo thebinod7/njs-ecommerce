481c52621a1005a2869670521020d1e68994bc3c
Binod Chaudhary <thebinod7@gmail.com>
2017-06-11 12:18:33 +0545

    cart and checkout UI

--- routes/user.js
+++ routes/user.js
@@ -3,16 +3,30 @@ const router = express.Router();
 
 router.get('/signup',function (req,res) {
     data = {
-        title: 'User Signup'
+        title: 'Signup - Onlne Store'
     },
         res.render('user/signup',data);
 });
 
 router.get('/login',function (req,res) {
     data = {
-        title: 'User Login'
+        title: 'Login - Online Store'
     },
         res.render('user/login',data);
+});
+
+router.get('/cart',function (req,res) {
+    data = {
+        title: 'Cart - Online Store'
+    },
+        res.render('user/cart',data);
+});
+
+router.get('/checkout',function (req,res) {
+    data = {
+        title: 'Checkout - Online Store'
+    },
+        res.render('user/checkout',data);
 });
 
 module.exports = router;
